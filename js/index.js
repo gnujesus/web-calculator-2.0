58b7346a0c4d405f06d21eb1203d2e649e60bdac
juandma0 <jotamartinezd@gmail.com>
2023-06-19 19:09:30 -0400

    improved screen behavior

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 let screen = document.querySelector(".screen");
+let screenFont = screen.style.fontSize;
 let buffer = "";
 let operand = "";
 let result = 0;
+
+function resetScreen(){
+    screen.textContent = 0;
+    screen.style.fontSize = "60px"
+}
 
 function handleOperation(value1, operation, value2){
     value1 = parseFloat(value1);
@@ -39,38 +45,45 @@ let btn = {
         switch (button){
             case "C":
                 screen.textContent = "0";
+                resetScreen();
                 break;
     
             case "←":
                 screen.textContent =  "0";
+                resetScreen();
                 break;
 
             case "÷":
                 buffer = screen.textContent;
-                screen.textContent = "0";
                 operand = "divide";
+                resetScreen();
                 break;
 
             case "×":
                 buffer = screen.textContent;
                 screen.textContent = "0";
                 operand = "times";
+                resetScreen();
                 break;
 
             case "−":
                 buffer = screen.textContent;
                 screen.textContent = "0";
                 operand = "minus";
+                resetScreen();
                 break;
 
             case "+":
                 buffer = screen.textContent;
                 screen.textContent = "0";
                 operand = "plus";
+                resetScreen();
                 break;
 
             case "=":
                 handleOperation(buffer, operand, screen.textContent);
+                resetScreen();
+                handleScreen(screen.textContent.length);
                 screen.textContent = result;
                 break;
 
@@ -81,13 +94,13 @@ let btn = {
                 break;
                 
             default:
-                if (screen.textContent == 0 || screen.textContent == result){
+                if (screen.textContent == 0 || screen.textContent == result || screen.textContent == "Error"){
                     screen.textContent = "";
                 }
                 screen.append(button);
                 break;
 
-        }
+            }
 
         // if (button === "C") {
         //     screen.textContent = "0";
@@ -97,12 +110,30 @@ let btn = {
         //     }
         //     screen.append(button);
         // }
+    },
+}
+
+function handleScreen(screenTextLength){
+
+    if (screen.textContent.length > 7 && screen.textContent.length < 11 ){
+        screen.style.fontSize = "45px";
+    } else if (screen.textContent.length > 11 && screen.textContent.length < 15) {
+        screen.style.fontSize = "35px";
+    } else if(screen.textContent.length > 14 && screen.textContent.length < 17){
+        screen.style.fontSize = "25px";
     }
+
+    // if (screen.textContent.length > 33){
+    //         screen.style.fontSize = "60px";
+    //         screen.textContent = "Error";
+    // }
+    return;
 }
 
 for (let i = 0; i < btn.value.length; i++) {
     btn.value[i].addEventListener("click", function (event) {
-        btn.handleButtonPress(event.target.textContent)
+        btn.handleButtonPress(event.target.textContent);
+        handleScreen(screen.textContent.length);
     })
 }
 
